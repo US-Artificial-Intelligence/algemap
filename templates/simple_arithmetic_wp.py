516c4dca27ad79687755d0b36d6b7949e8a599c2
Gordon Kamer <gkamer@outlook.com>
2023-06-22 14:52:10 -0700

    token counts

--- templates/simple_arithmetic_wp.py
+++ templates/simple_arithmetic_wp.py
@@ -1,5 +1,6 @@
 from .utils import Template, sample_from, sample_small_int
 import random
+import tiktoken
 
 """
 
@@ -15,10 +16,13 @@ class SimpleArithmeticWP(Template):
     def generate(self, *args):
         results = []
         for _ in range(10):
-            results.append(self.addition())
+            results.append(self.simple_two_num(kind="addition"))
+        for _ in range(10):
+            results.append(self.simple_two_num(kind="subtraction"))
         return results
 
-    def addition(self):
+    def simple_two_num(self, kind):
+
         num1 = sample_small_int()
         num2 = sample_small_int()
         is_boy = random.random() < .5
@@ -35,21 +39,36 @@ class SimpleArithmeticWP(Template):
         cap_pronoun, pronoun = ("He", "he") if is_boy else ("She", "she")
         q_prefix, a_prefix = sample_from("qa_prefixes")
 
-        add_verb = sample_from("add_verbs")
+        if kind == "addition":
+            verb = sample_from("add_verbs")
+            verb_follow_up = "" if random.random() < .5 else " more"
+            answer = num1 + num2
+        elif kind == "subtraction":
+            verb = sample_from("sub_verbs")
+            verb_follow_up = ""
+            answer = num1 - num2
+        else:
+            raise NotImplementedError(f"Word problem type '{kind}' not recognized.")
 
         repeat_noun = "" if random.random() < .5 else " " + plural
         say_now = "" if random.random() < .5 else " now"
         last_word = plural if random.random() < .5 else "of them"
 
-        answer = num1 + num2
-
-        add = f"""{q_prefix}{name} had {num1} {noun}. {cap_pronoun} {add_verb} {num2} more. How many{repeat_noun} does {pronoun} have now?
+        add = f"""{q_prefix}{name} had {num1} {noun}. {cap_pronoun} {verb} {num2}{verb_follow_up}. How many{repeat_noun} does {pronoun} have now?
 {a_prefix}{cap_pronoun}{say_now} has {answer} {last_word}."""
         
+        encoding = tiktoken.get_encoding("cl100k_base")
+        tokens = encoding.encode(add)
+
         metadata = {
-            'len': len(add),
+            'char_len': len(add),
             'answer_start_char': add.find(str(answer)),
-            'answer_len': len(str(answer))
+            'answer_char_len': len(str(answer)),
+            'tokens': tokens,
+            'tokenizer': "cl100k_base",
+            'tokens_len': len(tokens),
+            "word_problem_type": kind,
+            "shows_work": False
         }
 
         return add, metadata
